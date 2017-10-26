4ab694e31ce20cac8ef19ecc10801c1247ddfc23
baoyinghai <yhbao@amarsoft.com>
2017-10-26 15:16:07 +0800

    feat: v2.0

--- graph.js
+++ graph.js
@@ -4,25 +4,7 @@
 (function(global) {
 
 
-  //************ utils *************
-
-  function getRelativeLines(optionsLine, index) {
-    var ret = [];
-    optionsLine.forEach(function(item, i) {
-      if (item.s === index || item.e === index) {
-        ret.push({s: item.s, e: item.e, index: i});
-      }
-    });
-    return ret;
-  }
-
-
-  function myParseInt(v) {
-    if (!v) {
-      return 0;
-    }
-    return parseInt(v, 10);
-  }
+  // *************
 
   function getLeftMiddle(box) {
     return {
@@ -92,246 +74,361 @@
     }
 
   }
+  // **************
 
-  //*************************
+  // is connecting ?
+  var connect = false;
 
-  var dummyLine ;
-  var dragging = false;
-  var lineSx = 0;
-  var lineSy = 0;
-  var Graph = function(options) {
-    this.options = options || {};
-    this.d3 = options.d3;
-    if (!this.d3) throw new Error('need d3!');
-    this.boxs = options.boxs;
-    this.lines = options.lines;
-    this.container = d3.select('#' + options.id);
-    this.textSize = this.options.textSize || 12;
-    this.paddingLeft = this.options.textPaddingLeft || 0;
-    this.paddingTop = this.options.textPaddingTop || 0;
-    this.groups = this.options.groups || [];
-  };
+  function createSvgElement(type) {
+    return document.createElementNS("http://www.w3.org/2000/svg", type);
+  }
 
-  Graph.prototype.layoutInfo = function() {
-    return {
-      boxs: this.boxs,
-      lines: this.lines
-    };
-  };
+  function getTranslate(obj) {
+    var x,y;
+    var translate = obj.getAttribute('transform');
+    translate.split('(')[1].split(',').forEach(function(item, i) {
+      if (i === 0) {
+        x = parseInt(item.trim());
+      } else {
+        y = parseInt(item.trim());
+      }
+    });
 
-  Graph.prototype.offsetY = function(gindex, index) {
-    var ary = this.groups[gindex].ele;
-    var ret = 0;
-    for(var i=0; i < index;i++) {
-      ret += this.boxs[ary[i]].height;
-    }
-    return ret;
+    return { x: x, y: y };
+  }
 
-  };
+  function moveDecorator(obj) {
 
-  Graph.prototype.drawGroup = function(group, r) {
-    var self = this;
-    var groupWrapper = this.container.append('g');
-    groupWrapper
-      .attr('id', this.createGroupId(r))
-      .attr('transform', 'translate(' + group.x + ',' + group.y + ')');
-    var eles = group.ele;
-    var x = 0,
-      y = 0,
-      boxs = this.boxs,
-      textSize = this.textSize,
-      paddingLeft = this.paddingLeft,
-      paddingTop = this.paddingTop;
-    eles.forEach(function(item, i) {
-      var groupBox = groupWrapper.append('g');
-      groupBox.append('rect')
-        .attr('class', 'box')
-        .attr('x', x)
-        .attr('y', y)
-        .attr('width', boxs[item].width)
-        .attr('height', boxs[item].height);
-
-      // draw Text
-      groupBox.append('text')
-        .attr('class', 'text')
-        .attr('font-size', textSize + 'px')
-        .attr('x', paddingLeft) // TODO: 调整文字布局
-        .attr('y', y + textSize + paddingTop)
-        .text(boxs[item].text);
-
-      var handle = self.d3.behavior.drag()
-        .origin(function() {
-          return {};
-        });
-      handle.on('dragstart', function() {
-        var eCircle = self.d3.select(d3.event.sourceEvent.target);
-        lineSx = myParseInt(eCircle.attr('cx')) + group.x;
-        lineSy = myParseInt(eCircle.attr('cy')) + group.y;
-        dummyLine.attr('x1', lineSx).attr('y1', lineSy);
-        dragging = true;
-        self.d3.event.sourceEvent.stopPropagation();
-      });
-
-
-      handle.on('dragend', function() {
-        dragging = false;
-        // console.log('dragEnd1');
-        var eCircle = self.d3.select(d3.event.sourceEvent.target);
-        if (eCircle.attr('pid') !== null) {
-          self.addLine({s: item, e: myParseInt(eCircle.attr('pid'))});
+    var dragger = false;
+    var offsetX = 0;
+    var offsetY = 0;
+    var x,y;
+    obj.addEventListener('mousedown', function(event) {
+      var p = getTranslate(obj);
+      x = p.x;
+      y = p.y;
+      dragger = true;
+      offsetX = event.offsetX;
+      offsetY = event.offsetY;
+      obj.emit && obj.emit('movestart');
+      function moveHandle(event) {
+        if (dragger) {
+          obj.emit && obj.emit('moveing', {x: event.offsetX - offsetX, y: event.offsetY - offsetY});
+          obj.setAttribute('transform', 'translate(' + (x + event.offsetX - offsetX) + ',' + (y + event.offsetY - offsetY) + ')');
         }
-        dummyLine.style("display", "none");
-      });
-
-      var circleR = 2;
-
-      groupBox.append('circle')
-        .attr('class', 'pointer')
-        .attr('r', circleR)
-        .attr('cx', x)
-        .attr('cy', y + boxs[item].height / 2)
-        .attr('pid', item)
-        .call(handle);
-
-      groupBox.append('circle')
-        .attr('class', 'pointer')
-        .attr('r', circleR)
-        .attr('cx', x + boxs[item].width)
-        .attr('cy', y + boxs[item].height / 2)
-        .attr('pid', item)
-        .call(handle);
-
-
-      var groupHandle = this.d3.behavior.drag()
-        .origin(function() {
-          return {x: self.groups[r].x, y: self.groups[r].y };
-        });
-
-      groupHandle.on('dragstart', function() {
-        self.d3.event.sourceEvent.stopPropagation();
-      });
-
-      groupHandle.on('drag', function() {
-        groupWrapper.attr('transform', 'translate(' + d3.event.x + ',' + d3.event.y + ')');
-
-        // debugger;
-        self.groups[r].ele.forEach(function(index, gindex) {
-          getRelativeLines(self.lines, index).forEach(function(item) {
-            console.log('lineIndex:', item.index);
-            var line = self.d3.select('#' + self.createLineId(item.index));
-            var s = self.boxs[item.s];
-            var e = self.boxs[item.e];
-            // debugger;
-            if (item.s === index) {
-              s.x = myParseInt(self.d3.event.x);
-              s.y = myParseInt(self.d3.event.y + self.offsetY(r, gindex));
-            } else {
-              e.x = myParseInt(self.d3.event.x);
-              e.y = myParseInt(self.d3.event.y +  self.offsetY(r, gindex));
-            }
-            var newLine = getMinLine(s, e);
-            line.attr('x1', newLine.x1).attr('x2', newLine.x2).attr('y1', newLine.y1).attr('y2', newLine.y2);
-          })
-        });
-
-      });
-
-      groupHandle.on('dragend', function() {
-        var translate = self.getTransformXY(groupWrapper);
-        self.groups[r].x = translate.x;
-        self.groups[r].y = translate.y;
-        self.groups[r].ele.forEach(function(g, index) {
-          self.boxs[g].x = translate.x;
-          self.boxs[g].y = translate.y + self.offsetY(r, index);
-        })
-      });
-
-      if (group.moveable !== false) {
-        // add listener
-        groupWrapper.call(groupHandle);
       }
-      boxs[item].x = group.x + x;
-      boxs[item].y = group.y + y;
-      y+= boxs[item].height;
-
+      function upHandle(event) {
+        if (dragger) {
+          dragger = false;
+          var p = getTranslate(obj);
+          x = p.x;
+          y = p.y;
+        }
+        window.removeEventListener('mousemove', moveHandle);
+        window.removeEventListener('mouseup', upHandle);
+      }
+      window.addEventListener('mousemove', moveHandle);
+      window.addEventListener('mouseup', upHandle);
     });
-  };
+  }
 
-  Graph.prototype.display = function() {
+  function eventDecorator(obj, id) {
+    var listeners = {};
+
+    obj.on = function(type, handler) {
+      listeners[type] = handler;
+    };
+
+    obj.emit = function(type, data) {
+      listeners[type]&&listeners[type](data);
+    };
+  }
+
+  function connectDecorator(obj) {
+    var moveHandler = function(event) {
+      obj.emit('connecting', {x: event.offsetX-offsetX, y: event.offsetY-offsetY});
+    };
+    var upHandler = function() {
+      if(connect) {
+        obj.setAttribute('class', 'pointer');
+        connect = false;
+      }
+      window.removeEventListener('mousemove', moveHandler);
+      window.removeEventListener('mouseup', upHandler);
+      obj.emit('connectrelease');
+    };
+    var offsetX,offsetY;
+    obj.addEventListener('mousedown', function(event) {
+      event.stopPropagation();
+      connect = true;
+      obj.emit('connectstart');
+      offsetX = event.offsetX;
+      offsetY = event.offsetY;
+      window.addEventListener('mousemove', moveHandler);
+      window.addEventListener('mouseup', upHandler)
+    });
+
+    obj.addEventListener('mouseover', function(event) {
+      obj.setAttribute('class', 'pointer blue');
+    });
+
+    obj.addEventListener('mouseout', function(event) {
+      if (!connect) {
+        obj.setAttribute('class', 'pointer');
+      }
+    });
+
+    obj.addEventListener('mouseup', function(event) {
+      if (connect) {
+        console.log('mouseup!!!!');
+        obj.emit('connectend');
+      }
+    });
+
+  }
+
+
+  var Group = function(options, id, boxs, groupMoveStartHandle, groupMovingHandle, connectStartHandler, connectingHandler, connectendHandler) {
+    /**
+     * options {x,y}
+     * */
+    this.options = options;
+    this.id = id;
+    var g = createSvgElement("g");
+    g.setAttribute('transform', 'translate(' + options.x + ',' + options.y + ')');
+    eventDecorator(g, id);
+    moveDecorator(g);
+    this.node = g;
+
+    var offset = 0;
+    this.boxs = options.ele.map(function(boxId) {
+      var b = new Box(boxs[boxId], offset, connectStartHandler, connectingHandler, connectendHandler, boxId);
+      offset += boxs[boxId].height;
+      return b;
+    });
+
     var self = this;
+    g.on('movestart', function() {
+      groupMoveStartHandle(id);
+    });
 
-    if (this.groups.length > 0) {
-      for (var r = 0; r < this.groups.length; r++) {
-        this.drawGroup(this.groups[r], r);
-      }
-    } else {
-      for(var i = 0; i < this.boxs.length ; i++) {
-        this.drawBox(this.boxs[i], i);
-      }
-    }
-
-    for(var j = 0; j<this.lines.length ;j++) {
-      this.drawLine(this.lines[j], j);
-    }
-
-    dummyLine = this.container.append('line');
-    dummyLine
-      .attr('class', 'dummy')
-      .attr('x1', 0)
-      .attr('y1', 0)
-      .attr('x2', 0)
-      .attr('y2', 0);
-
-
-    this.container.on('mousemove', function() {
-      var rad = 7;
-      dummyLine.attr('x2', self.d3.event.x - rad).attr('y2', self.d3.event.y - rad);
-      if (dragging) {
-        dummyLine.style("display", "block")
-      }
+    g.on('moveing', function(data) {
+      groupMovingHandle(data.x,data.y)
     });
 
   };
 
-  Graph.prototype.transformLine2Coordinate = function(line) {
-    return getMinLine(this.boxs[line.s], this.boxs[line.e]);
+  Group.prototype.render = function(container) {
+    var node = this.node;
+    this.boxs.forEach(function(b) {
+      b.render(node);
+    });
+    container.appendChild(node);
   };
 
-  Graph.prototype.createLineId = function(i) {
-    return 'line_' + i;
-  };
-
-  Graph.prototype.createGroupId = function(i) {
-    return 'group_' + i;
-  };
-
-  Graph.prototype.drawLine = function(line, i) {
-    var dline = this.transformLine2Coordinate(line);
-    this.container.append('line')
-      .attr('id', this.createLineId(i))
-      .attr('class', 'line')
-      .attr('x1', dline.x1)
-      .attr('y1', dline.y1)
-      .attr('x2', dline.x2)
-      .attr('y2', dline.y2);
-  };
-
-  Graph.prototype.createBoxId = function(i) {
-    return 'box_' + i;
-  };
-
-  Graph.prototype.getTransformXY = function(boxWrapper) {
-    var translate = this.d3.transform(boxWrapper.attr("transform")).translate;
-    return { x: myParseInt(translate[0]), y: myParseInt(translate[1])};
-  };
-
-  Graph.prototype.addLine = function(line) {
-    if (this.lines.filter(function(item){
-        return (item.s == line.s && item.e === line.e) || (item.s == line.e && item.e === line.s)
-      }).length === 0) {
-      this.lines.push(line);
-      this.drawLine(line, this.lines.length - 1);
+  Group.prototype.getBox = function(boxId) {
+    var box = this.boxs[this.options.ele.indexOf(boxId)];
+    var p = getTranslate(this.node);
+    return {
+      y: box.offset + p.y,
+      x: p.x,
+      height: box.height,
+      width: box.width
     }
+  };
+
+  Group.prototype.hasBox = function(boxId) {
+    return this.options.ele.indexOf(boxId) !== -1;
+  };
+
+  var Box = function(options, offset, connectStartHandler, connectingHandler, connectendHandler, id) {
+    /**
+     * options {height, width, id}
+     * */
+//      this.options = options;
+    this.height = options.height;
+    this.width = options.width;
+    var box = createSvgElement("rect");
+    box.setAttribute('class', 'box');
+    box.setAttribute('x', 0);
+    box.setAttribute('y', offset);
+    box.setAttribute('height', this.height);
+    box.setAttribute('width', this.width);
+    this.node = box;
+    this.offset = offset;
+
+
+    this.pointerLeft = new ConnectPoint({x: 0, y: offset + this.height / 2}, connectStartHandler, connectingHandler, connectendHandler, id);
+    this.pointerRight = new ConnectPoint({x: this.width, y: offset + this.height / 2}, connectStartHandler, connectingHandler, connectendHandler, id);
+  };
+
+  Box.prototype.render = function(group) {
+    group.appendChild(this.node);
+    this.pointerLeft.render(group);
+    this.pointerRight.render(group);
+  };
+
+  var Line = function(options) {
+    this.s = options.s;
+    this.e = options.e;
+    var l = createSvgElement("line");
+    l.setAttribute('class', 'line');
+    l.setAttribute('marker-end', 'url(#arrow)');
+    l.setAttribute('x1', 0);
+    l.setAttribute('y1', 0);
+    l.setAttribute('x2', 200);
+    l.setAttribute('y2', 200);
+    this.node = l;
+    this.tmpX = 0;
+    this.tmpY = 0;
+  };
+
+  Line.prototype.dummyLine = function() {
+    this.node.setAttribute('class', 'dummy');
+  };
+
+  Line.prototype.visible = function() {
+    this.node.setAttribute('class', 'dummy visible');
+  };
+
+  Line.prototype.render = function(container, p) {
+    this.node.setAttribute('x1', p.x1);
+    this.node.setAttribute('y1', p.y1);
+    this.node.setAttribute('x2', p.x2);
+    this.node.setAttribute('y2', p.y2);
+    container.appendChild(this.node);
+  };
+
+  Line.prototype.setStartXY = function(x, y) {
+    this.node.setAttribute('x1', x);
+    this.node.setAttribute('y1', y);
+    this.node.setAttribute('x2', x);
+    this.node.setAttribute('y2', y);
+    this.tmpX = x;
+    this.tmpY = y;
+  };
+
+  Line.prototype.startMove = function(direct) {
+    this.tmpX = parseInt(this.node.getAttribute('x' + direct));
+    this.tmpY = parseInt(this.node.getAttribute('y' + direct));
+  };
+
+  Line.prototype.updateXY = function(direct, x, y) {
+
+    this.node.setAttribute('x' + direct, this.tmpX + x);
+    this.node.setAttribute('y' + direct, this.tmpY + y);
+  };
+
+  var ConnectPoint = function(options, connectStartHandler, connectingHandler, connectendHandler, id) {
+    this.options = options;
+    var circle = createSvgElement("circle");
+    circle.setAttribute('class', 'pointer');
+    circle.setAttribute('r', 5);
+    circle.setAttribute('cx', options.x);
+    circle.setAttribute('cy', options.y);
+    eventDecorator(circle);
+    connectDecorator(circle);
+    this.node = circle;
+    circle.on('connectstart', function() {
+      connectStartHandler(id, options.x, options.y)
+    });
+    circle.on('connecting', function(data) {
+      connectingHandler(data)
+    });
+    circle.on('connectrelease', function(data) {
+      connectendHandler();
+    });
+    circle.on('connectend', function() {
+      connectendHandler(id);
+    })
+  };
+
+  ConnectPoint.prototype.render = function(group) {
+    group.appendChild(this.node);
+  };
+
+  var Graph = function(options) {
+    this.container = document.querySelector('#container');
+    this.boxs = options.boxs;
+    var self = this;
+    this.dummyLine = new Line({});
+    this.dummyLine.dummyLine();
+    this.groups = options.groups.map(function(group, index) {
+      return new Group(group, index, options.boxs, groupMoveStartHandle, groupMovingHandle, connectStartHandler, connectingHandler, connectendHandler);
+    });
+    this.lines = options.lines.map(function(line) {
+      return new Line(line);
+    });
+
+    this.effectLines = [];
+
+    function groupMoveStartHandle(groupId) {
+      self.effectLines = [];
+      // 计算出受影响的line
+      var boxList = options.groups[groupId].ele;
+      self.lines.forEach(function(line) {
+
+        if (boxList.indexOf(line.s) !== -1) {
+          self.effectLines.push({insc: line, direct: '1'});
+          line.startMove('1');
+        } else if (boxList.indexOf(line.e) !== -1) {
+          self.effectLines.push({insc: line, direct: '2'});
+          line.startMove('2');
+        }
+      });
+    };
+
+    function groupMovingHandle(x, y) {
+      self.effectLines.forEach(function(line) {
+        line.insc.updateXY(line.direct, x, y);
+      })
+    }
+    this.startBox = 0;
+    this.endBox = 0;
+    function connectStartHandler(id, x, y) {
+      var box = self.getBox(id);
+      self.startBox = id;
+      self.dummyLine.setStartXY(box.x + x, box.y + box.height / 2);
+      self.dummyLine.visible();
+    }
+
+    function connectingHandler(data) {
+      self.dummyLine.updateXY('2', data.x,data.y);
+    }
+
+    function connectendHandler(id) {
+
+      self.dummyLine.dummyLine();
+      if (id !== self.startBox && id !== undefined) {
+        self.endBox = id;
+        var newLine = {s: self.startBox, e: id};
+        options.lines.push(newLine);
+        var newLineInstance = new Line(newLine);
+        self.lines.push(newLineInstance);
+        newLineInstance.render(self.container, getMinLine(self.getBox(self.startBox), self.getBox(id)));
+      }
+    }
+  };
+
+  Graph.prototype.getBox = function(boxId) {
+    console.log(boxId);
+    if (boxId === undefined) {
+      debugger;
+    }
+    return this.groups.filter(function(g) {
+      return g.hasBox(boxId)
+    })[0].getBox(boxId);
+  };
+
+  Graph.prototype.render = function() {
+    this.groups.forEach(function(group) {
+      group.render(this.container);
+    });
+    var self = this;
+    this.lines.forEach(function(line) {
+      line.render(this.container, getMinLine(self.getBox(line.s), self.getBox(line.e)));
+    });
+    this.dummyLine.render(this.container, { x1: 0, y1: 0, x2: 0, y2: 0 });
   };
 
 
